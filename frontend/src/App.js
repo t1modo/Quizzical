77e0cfd539246a689c4bf9c4a2d6e97fcbff8078
Timothy Do <timothy.doK2004@gmail.com>
2024-11-09 21:45:34 -0800

    updated logo and added open api key

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import axios from 'axios';
-import logo from './assets/quiz_me_logo.png';
+import logo from './assets/quizzical.svg';
 import feather from 'feather-icons';
 
 const Popup = ({ isOpen, onClose, selectedOptions, onSelectOption, onSliderChange }) => {
